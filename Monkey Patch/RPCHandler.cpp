f292642ecb385ce374a9bff2bf45d0061dc9d2bf
Uzis <danielkull@live.com.au>
2024-08-10 03:15:02 +1000

    Slight code cleanup, Map detection for rich presence

--- Monkey Patch/RPCHandler.cpp
+++ Monkey Patch/RPCHandler.cpp
@@ -35,12 +35,6 @@ namespace RPCHandler {
 		return converter.to_bytes(wstr);
 	}
 
-	void checkishost() {
-
-		int player_offset = *(int*)0x21703D4;
-		int check = hostcheck(player_offset);
-		Logger::TypedLog(CHN_DEBUG, "Are We Host?: %x\n", check);
-	}
 
 	void DiscordCallbacks()
 	{
@@ -49,8 +43,125 @@ namespace RPCHandler {
 
 	bool IsCoopOrSP = false;
 	bool ShouldFixStereo = false;
+	const char *FancyChunkName[2048];
 
-	void UpdateDiscordParams() { // Updates state info for discord.
+	int mapListAPI(const char* word) {
+		if (strcmp(word, "sr2_mp_lobby") == 0) return 1;
+		if (strcmp(word, "sr2_mp_gb_frat01") == 0) return 2;
+		if (strcmp(word, "sr2_mp_gb_yacht1") == 0) return 3;
+		if (strcmp(word, "sr2_mp_gb_dome01") == 0) return 4;
+		if (strcmp(word, "sr2_mp_gb_ultorB") == 0) return 5;
+		if (strcmp(word, "sr2_mp_gb_hangar") == 0) return 6;
+		if (strcmp(word, "sr2_mp_gb_ct") == 0) return 7;
+		if (strcmp(word, "sr2_mp_gb_pyramid") == 0) return 8;
+		if (strcmp(word, "sr2_mp_gb_ug_mall") == 0) return 9;
+		if (strcmp(word, "sr2_mp_gb_st") == 0) return 10;
+		if (strcmp(word, "sr2_mp_gb_sci_mu") == 0) return 11;
+		if (strcmp(word, "sr2_mp_gb_kanto1") == 0) return 12;
+		if (strcmp(word, "sr2_mp_gb_wareh1") == 0) return 13;
+		if (strcmp(word, "sr2_mp_sa_ct") == 0) return 14;
+		if (strcmp(word, "sr2_mp_sa_marina") == 0) return 15;
+		if (strcmp(word, "sr2_mp_sa_nuke") == 0) return 16;
+		if (strcmp(word, "sr2_mp_sa_ba2") == 0) return 17;
+		if (strcmp(word, "sr2_mp_sa_museum2") == 0) return 18;
+		if (strcmp(word, "sr2_mp_sa_casino") == 0) return 19;
+		if (strcmp(word, "sr2_mp_sa_docks") == 0) return 20;
+		if (strcmp(word, "sr2_mp_gb_demo01") == 0) return 21;
+		if (strcmp(word, "sr2_mp_lobby02") == 0) return 22;
+		if (strcmp(word, "sr2_mp_lobby03") == 0) return 23;
+		if (strcmp(word, "sr2_mp_lobby04") == 0) return 24;
+		if (strcmp(word, "sr2_mp_lobbytut") == 0) return 25;
+		return 0;
+	}
+
+	void MapDescAPI() {
+		char* chunkID = (CHAR*)0x00E99740;
+		int code = mapListAPI(chunkID);
+
+		switch (code) {
+		case 1:
+			*FancyChunkName = "SR1 Lobby";
+			break;
+		case 2:
+			*FancyChunkName = "Frat House Lobby";
+			break;
+		case 3:
+			*FancyChunkName = "Ultor Yacht";
+			break;
+		case 4:
+			*FancyChunkName = "Ultor Dome";
+			break;
+		case 5:
+			*FancyChunkName = "Ultor Ball Room";
+			break;
+		case 6:
+			*FancyChunkName = "The Hangar";
+			break;
+		case 7:
+			*FancyChunkName = "Little Shanghai";
+			break;
+		case 8:
+			*FancyChunkName = "The Pyramid";
+			break;
+		case 9:
+			*FancyChunkName = "Rounds Square Mall";
+			break;
+		case 10:
+			*FancyChunkName = "Shantytown";
+			break;
+		case 11:
+			*FancyChunkName = "Stilwater Science";
+			break;
+		case 12:
+			*FancyChunkName = "Kanto";
+			break;
+		case 13:
+			*FancyChunkName = "Warehouse";
+			break;
+		case 14:
+			*FancyChunkName = "Little Shanghai";
+			break;
+		case 15:
+			*FancyChunkName = "North Shore";
+			break;
+		case 16:
+			*FancyChunkName = "Nuke Plant";
+			break;
+		case 17:
+			*FancyChunkName = "Oldtown";
+			break;
+		case 18:
+			*FancyChunkName = "Partheon";
+			break;
+		case 19:
+			*FancyChunkName = "Poseidon's Palace";
+			break;
+		case 20:
+			*FancyChunkName = "Wharfside";
+			break;
+		case 21:
+			*FancyChunkName = "Demolition Derby";
+			break;
+		case 22:
+			*FancyChunkName = "Purgatory Crib Lobby";
+			break;
+		case 23:
+			*FancyChunkName = "Gymnasium Lobby";
+			break;
+		case 24:
+			*FancyChunkName = "Hangar Lobby";
+			break;
+		case 25:
+			*FancyChunkName = "Tutorial Lobby";
+			break;
+		default:
+			*FancyChunkName = chunkID;
+			break;
+		}
+	}
+
+	// Updates state info for discord.
+	void UpdateDiscordParams() { 
 		BYTE CurrentGamemode = *(BYTE*)0x00E8B210; // Parses the current gamemode from EXE
 		BYTE LobbyCheck = *(BYTE*)0x02528C14; // Checks lobby, technically this is another gamemode check but we'll use it for lobby
 		BYTE MatchType = *(BYTE*)0x00E8B20C; // Checks match type
@@ -58,23 +169,23 @@ namespace RPCHandler {
 		wchar_t* partnerName = (WCHAR*)0x02CD1870; // parses Co-op Partner name, usually
 		BYTE AbleToStartGame = *(BYTE*)0x02528D90; // Determines whether the gamemode is able to start or not (we'll force this on when we can, nice QOL feature.)
 		BYTE IsInCutscene = *(BYTE*)0x02527D14; // Checks if user is in a cutscene.
-		//BYTE IsHost = *(BYTE*)0x02528C14; // checks a dword if host.
 
 		std::wstring wPartnerName = partnerName; // parse co-op partner name to a wstring
 		std::string f_PartnerName = wstring_to_string(wPartnerName); // THEN to a string
 		const char* COOPPartner = f_PartnerName.c_str(); // now to a const char because discord is a picky bitch
 		char finalUsername[2048];
+		char finalMPDesc[2048];
+		char finalCOOPDescCutsc[2048];
+		char finalCOOPDesc[2048];
 
 	    sprintf(finalUsername, "Username: %s", playerName);
-		char finalCOOPDescCutsc[2048];
+		sprintf(finalMPDesc, "Username: %s | In Map: %s", playerName, *FancyChunkName);
 		sprintf(finalCOOPDescCutsc, "Watching a Cutscene with %s", COOPPartner);
-		char finalCOOPDesc[2048];
 		sprintf(finalCOOPDesc, "Playing CO-OP with %s", COOPPartner);
 
 		static DWORD lastTick = 0;
 
 		DWORD currentTick = GetTickCount();
-		//Logger::TypedLog(CHN_DEBUG, "Are We Host?: %x\n", IsHost);
 		if (currentTick - lastTick >= 600) {
 			lastTick = currentTick;
 			if (!LobbyCheck == 0x0 && CurrentGamemode == 0xFF) // This should be CO-OP / Singleplayer
@@ -91,7 +202,7 @@ namespace RPCHandler {
 					}
 					else 
 					{
-						strcpy_s(pres.details, sizeof(pres.details) / sizeof(pres.details[0]) , finalCOOPDescCutsc);
+						strcpy_s(pres.details, finalCOOPDescCutsc);
 						strcpy_s(pres.state, finalUsername);
 					}
 					IsCoopOrSP = true;
@@ -120,6 +231,7 @@ namespace RPCHandler {
 			}
 			if (CurrentGamemode == 0xD) // Strong Arm
 			{
+				MapDescAPI();
 				if (MatchType == (BYTE)1)
 					strcpy_s(pres.details, "Playing Player MP in Strong Arm");
 
@@ -129,10 +241,11 @@ namespace RPCHandler {
 				if (MatchType == (BYTE)3)
 					strcpy_s(pres.details, "Playing Party MP in Strong Arm");
 
-				strcpy_s(pres.state, finalUsername);
+				strcpy_s(pres.state, finalMPDesc);
 			}
 			if (CurrentGamemode == 0xB) // Gangsta Brawl
 			{
+				MapDescAPI();
 				if (MatchType == (BYTE)1)
 					strcpy_s(pres.details, "Playing Player MP in Gangsta Brawl");
 
@@ -142,10 +255,11 @@ namespace RPCHandler {
 				if (MatchType == (BYTE)3)
 					strcpy_s(pres.details, "Playing Party MP in Gangsta Brawl");
 
-				strcpy_s(pres.state, finalUsername);
+				strcpy_s(pres.state, finalMPDesc);
 			}
 			if (CurrentGamemode == 0xC) // Team Gangsta Brawl
 			{
+				MapDescAPI();
 				if (MatchType == (BYTE)1)
 					strcpy_s(pres.details, "Playing Player MP in Team Gangsta Brawl");
 
@@ -155,11 +269,10 @@ namespace RPCHandler {
 				if (MatchType == (BYTE)3)
 					strcpy_s(pres.details, "Playing Party MP in Team Gangsta Brawl");
 
-				strcpy_s(pres.state, finalUsername);
+				strcpy_s(pres.state, finalMPDesc);
 			}
 			if (LobbyCheck == 0x44) // Game Lobby
 			{
-				//checkishost();
 				if (MatchType == (BYTE)2) { // If in ranked
 					if (!CurrentGamemode == 0xD || !CurrentGamemode == 0xC || CurrentGamemode == 0xB) // And gamemode is not TGB or Strong Arm but is Gangsta Brawl
 					{
@@ -191,8 +304,6 @@ namespace RPCHandler {
 			*(BYTE*)0x02528D90 = AbleToStartGame;
 
 			app.activities->update_activity(app.activities, &pres, 0, 0);
-
-			//Logger::TypedLog(CHN_DEBUG, "CurrentGamemode: %x, LobbyCheck: %x, PlayerName: %s, AbleToStartGame: %x\n", CurrentGamemode, LobbyCheck, playerName, AbleToStartGame);
 		}
 
 	}
@@ -203,7 +314,6 @@ namespace RPCHandler {
 		BYTE MatchType = *(BYTE*)0x00E8B20C; // Checks match type
 		BYTE AbleToStartGame = *(BYTE*)0x02528D90; // Determines whether the gamemode is able to start or not (we'll force this on when we can, nice QOL feature.)
 		BYTE IsInCutscene = *(BYTE*)0x02527D14; // Checks if user is in a cutscene.
-
 
 		static DWORD lastTick = 0;
 
@@ -271,7 +381,6 @@ namespace RPCHandler {
 		params.activity_events = &activities_events;
 		params.relationship_events = &relationships_events;
 		params.user_events = &users_events;
-		//DiscordCreate(DISCORD_VERSION, &params, &app.core);
 		int fail = DiscordCreate(DISCORD_VERSION, &params, &app.core);
 		if (fail)
 		{
@@ -296,10 +405,8 @@ namespace RPCHandler {
 				//Initialize a basic Rich Presence.
 				pres.timestamps.start = CurrentTime;
 				//pres.timestamps.end = 0;
-				//strcpy_s(pres.details, "Reclaiming Stilwater...");
 				strcpy_s(pres.assets.large_image, "sr2");
 				app.activities->update_activity(app.activities, &pres, 0, 0);
-				//ChangeDetails("test");
 				// ---------------------------------
 			}
 		}
@@ -340,11 +447,8 @@ namespace RPCHandler {
 				std::time_t CurrentTime = std::time(0);
 				//Initialize a basic Rich Presence.
 				pres.timestamps.start = CurrentTime;
-				//pres.timestamps.end = 0;
-				//strcpy_s(pres.details, "Indev 0.0.0.1");
 				strcpy_s(pres.assets.large_image, "sr2mprr");
 				app.activities->update_activity(app.activities, &pres, 0, 0);
-				//ChangeDetails("test");
 				// ---------------------------------
 			}
 		}
@@ -363,13 +467,5 @@ namespace RPCHandler {
 		// does it immediately upon exiting game sometimes
 		// (unless that's because i don't have the game
 		// manually added in app settings)
-	}
-
-	void AddFunctions()
-	{
-		if (Enabled)
-		{
-			//CFuncs::RedirectFunction("SetRichPresenceMode", (void*)CFunc_SetRichPresenceMode);
-		}
 	}
 }
