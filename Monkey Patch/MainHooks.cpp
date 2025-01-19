c8a8b16157e5d92ceb67acc0ebba5b57e9a0f7a1
Uzis <danielkull@live.com.au>
2025-01-19 22:56:30 +1100

    moved some printing to AssetLoadPrint, fix stencil shadows, check if user has custom shaders then disable filtering

--- Monkey Patch/MainHooks.cpp
+++ Monkey Patch/MainHooks.cpp
@@ -1807,6 +1807,21 @@ bool FileExists(const char* fileName) {
 	return found;
 }
 
+int dirExists(const char* const path)
+{
+	struct stat info;
+
+	int statRC = stat(path, &info);
+	if (statRC != 0)
+	{
+		if (errno == ENOENT) { return 0; } // something along the path does not exist
+		if (errno == ENOTDIR) { return 0; } // something in path prefix is not a dir
+		return -1;
+	}
+
+	return (info.st_mode & S_IFDIR) ? 1 : 0;
+}
+
 int WINAPI Hook_WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
 {
 	Logger::TypedLog(CHN_DLL, "SetProcessDPIAware result: %s\n", SetProcessDPIAware() ? "TRUE" : "FALSE");
@@ -1824,6 +1839,7 @@ int WINAPI Hook_WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCm
 	patchBytesM((BYTE*)0x0053818F, (BYTE*)"\xA1\x94\x89\xE9\x00", 5); // make shadow maps check shadows instead of shadow map type
 	patchBytesM((BYTE*)0x00538194, (BYTE*)"\x83\xE8\x02", 3); // make it check if full shadows are enabled (so none = no shadows, simple = stencil and full = stencil & s. maps)
 	patchNop((BYTE*)0x0077376D, 3); // force full stencil shadows with the simple setting, removes the == 2 check
+	patchNop((BYTE*)0x0077377B, 3); // fixes an issue with some users and the nop above.
 	patchNop((BYTE*)0x006C5FE0, 10); // fix cutscenes resetting shadows
 	patchNop((BYTE*)0x0073C01B, 6); // remove the command check from the level function
 	patchCall((void*)0x00458646, (void*)IdleFix); // prevents you from being able to use the scroll wheel when idling
@@ -1842,7 +1858,13 @@ int WINAPI Hook_WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCm
 	//FixandImproveSlewMouseRuntimePatch();
 	WriteRelJump(0x0098E493, (UInt32)&StoreNPCPointer);
 	WriteRelJump(0x0098EE0B, (UInt32)&SpawningCheck);
-	WriteRelJump(0x0051596E, (UInt32)&RestoreFiltering);
+	if (!dirExists("./shaderoverride")) {
+		Logger::TypedLog(CHN_DLL, "We can patch 2x Filtering.\n");
+		WriteRelJump(0x0051596E, (UInt32)&RestoreFiltering);
+	}
+	else {
+		Logger::TypedLog(CHN_DLL,"Not patching 2x Filtering.\n");
+	}
 	ErrorManager::Initialize();
 	char NameBuffer[260];
 	PIMAGE_DOS_HEADER dos_header;
--- Monkey Patch/UGC/InternalPrint.cpp
+++ Monkey Patch/UGC/InternalPrint.cpp
@@ -69,14 +69,29 @@ namespace InternalPrint
 			patchCall((void*)0x00C0ADEF, (void*)sprintfHook);
 			patchNop((BYTE*)0x00C0AD26, 5); // asset load
 			patchCall((void*)0x00C0AD26, (void*)sprintfHook);
+			patchNop((BYTE*)0x00CF4C42, 5); // is not a shader file
+			patchCall((void*)0x00CF4C42, (void*)sprintfHook);
+			patchNop((BYTE*)0x00CF4C70, 5); // is out of date
+			patchCall((void*)0x00CF4C70, (void*)sprintfHook);
+			patchNop((BYTE*)0x00D19162, 5); // shader loaded
+			patchCall((void*)0x00D19162, (void*)sprintfHook);
+			patchNop((BYTE*)0x00A842AB, 5); // new anim loaded
+			patchCall((void*)0x00A842AB, (void*)sprintfHook);
+			patchNop((BYTE*)0x0069C703, 5); // list of city chunks loaded
+			patchCall((void*)0x0069C703, (void*)sprintfHook);
+			patchNop((BYTE*)0x0048130E, 5); // Music Wavebanks Loaded
+			patchCall((void*)0x0048130E, (void*)sprintfHook);
+			patchNop((BYTE*)0x00481374, 5); // Sound Wavebanks Loaded
+			patchCall((void*)0x00481374, (void*)sprintfHook);
+			patchNop((BYTE*)0x004813B7, 5); // Soundbanks Loaded
+			patchCall((void*)0x004813B7, (void*)sprintfHook);
+			patchNop((BYTE*)0x004A4929, 5); // Character tables Loaded
+			patchCall((void*)0x004A4929, (void*)sprintfHook);
+			patchNop((BYTE*)0x00479869, 5); // Audio Loaded
+			patchCall((void*)0x00479869, (void*)sprintfHook);
+			patchNop((BYTE*)0x00A253CA, 5); // loaded map lua
+			patchCall((void*)0x00A253CA, (void*)sprintfHook);
 		}
-
-		patchNop((BYTE*)0x00CF4C42, 5); // is not a shader file
-		patchCall((void*)0x00CF4C42, (void*)sprintfHook);
-		patchNop((BYTE*)0x00CF4C70, 5); // is out of date
-		patchCall((void*)0x00CF4C70, (void*)sprintfHook);
-		patchNop((BYTE*)0x00D19162, 5); // shader loaded
-		patchCall((void*)0x00D19162, (void*)sprintfHook);
 
 		patchNop((BYTE*)0x00541D39, 5); // chunk swapped?
 		patchCall((void*)0x00541D39, (void*)sprintfHook);
@@ -85,28 +100,10 @@ namespace InternalPrint
 
 		patchNop((BYTE*)0x00CE5600, 5); // anim not paged in
 		patchCall((void*)0x00CE5600, (void*)sprintfHook);
-		patchNop((BYTE*)0x00A842AB, 5); // new anim loaded
-		patchCall((void*)0x00A842AB, (void*)sprintfHook);
-		patchNop((BYTE*)0x00A253CA, 5); // loaded map lua
-		patchCall((void*)0x00A253CA, (void*)sprintfHook);
 		patchNop((BYTE*)0x00520FAE, 5); // Savegame location
 		patchCall((void*)0x00520FAE, (void*)sprintfHook);
-		patchNop((BYTE*)0x0069C703, 5); // list of city chunks loaded
-		patchCall((void*)0x0069C703, (void*)sprintfHook);
 		patchNop((BYTE*)0x004A5513, 5); // Character error warning
 		patchCall((void*)0x004A5513, (void*)sprintfHook);
-		patchNop((BYTE*)0x004A4929, 5); // Character tables Loaded
-		patchCall((void*)0x004A4929, (void*)sprintfHook);
-
-		patchNop((BYTE*)0x0048130E, 5); // Music Wavebanks Loaded
-		patchCall((void*)0x0048130E, (void*)sprintfHook);
-		patchNop((BYTE*)0x00481374, 5); // Sound Wavebanks Loaded
-		patchCall((void*)0x00481374, (void*)sprintfHook);
-		patchNop((BYTE*)0x004813B7, 5); // Soundbanks Loaded
-		patchCall((void*)0x004813B7, (void*)sprintfHook);
-
-		patchNop((BYTE*)0x00479869, 5); // Audio Loaded
-		patchCall((void*)0x00479869, (void*)sprintfHook);
 
 		patchNop((BYTE*)0x008CBF79, 5); // GS Debugging
 		patchCall((void*)0x008CBF79, (void*)vsprintfHook);
