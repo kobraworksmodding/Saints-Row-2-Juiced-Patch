60c887146344899af73170cbf6187b260883b4a5
Clippy95 <qpzezoqp15@gmail.com>
2025-03-18 05:52:21 +0300

    Ignore BuildInfo.h. done in a seperate commit so at least have a base to build.

--- Monkey Patch/Generated/BuildInfo.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-// Auto-generated file - Do not edit directly
-#include <string>
-// Git commit hash
-inline const char* GIT_COMMIT_HASH = "9d7180494bfa1965a981d97b1f9b85c955faf7bd";
-// Build date and time in UTC
-inline const char* BUILD_TIME_UTC = "2025-03-18 02:50:56 UTC";
