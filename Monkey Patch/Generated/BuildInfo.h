469d960ad28186a18f48062895b9f3c41e891b8c
Clippy95 <qpzezoqp15@gmail.com>
2025-03-23 18:23:26 +0300

    Create BuildInfo.h

---
+++ Monkey Patch/Generated/BuildInfo.h
@@ -0,0 +1,7 @@
+#pragma once
+// Auto-generated file - Do not edit directly
+#include <string>
+// Git commit hash
+inline const char* GIT_COMMIT_HASH = "ea198e4a955e6a00dc661d6e0738834e4809273d";
+// Build date and time in UTC
+inline const char* BUILD_TIME_UTC = "2025-03-23 15:23:14 UTC";
