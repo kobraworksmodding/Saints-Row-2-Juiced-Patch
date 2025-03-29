d205da504984fd0ec2eb8a6d2e50a3b58b4bd85e
Clippy95 <qpzezoqp15@gmail.com>
2025-03-29 23:39:19 +0300

    POC

--- Monkey Patch/LUA/GLua.cpp
+++ Monkey Patch/LUA/GLua.cpp
@@ -5,6 +5,10 @@
 #include "GLua.h"
 #include "..\LuaHandler.h"
 
+#include "../Render/Render3D.h"
+
+#include <safetyhook.hpp>
+
 typedef void(__stdcall* NeverDieT)(int character,uint8_t status);
 NeverDieT NeverDie = (NeverDieT)0x00966720;
 
@@ -28,9 +32,67 @@ namespace GLua
 
         return 0; 
     }
+    int __cdecl lua_func_vint_get_avg_processing_time(lua_State* L) {
+        if (L == NULL) {
+            return 0;
+        }
+        const char* cmd = lua_tostring(L, 1);
+        if (cmd == NULL) {
+            lua_pushnil(L);
+            return 0;
+        }
+        if (strcmp(cmd, "ReadInt") == 0) {
+            unsigned int address = (unsigned int)lua_tonumber(L, 2);
+            int value = *(int*)address;
+            lua_pushnumber(L, value);
+            Logger::TypedLog(CHN_LUA, "game called %s it read 0x%X and got %d", "ReadInt", address, value);
+            return 1;
+        }
+        else if (strcmp(cmd, "WriteInt") == 0) {
+            unsigned int address = (unsigned int)lua_tonumber(L, 2);
+            int value = (int)lua_tonumber(L, 3);
+            *(int*)address = value;
+            lua_pushboolean(L, 1); // Success
+            return 1;
+        }
+        else if (strcmp(cmd, "ReadJuiced") == 0) {
+            const char* varName = lua_tostring(L, 2);
+            if (varName == NULL) {
+                lua_pushnil(L);
+                return 1;
+            }  
+            if (strcmp(varName, "VFXPlus") == 0) {
+                int value = Render3D::CMPatches_VFXPlus.IsApplied();
+                lua_pushnumber(L, value);
+            }
+            return 1;
+        }
+        else if (strcmp(cmd, "WriteJuiced") == 0) {
+            const char* varName = lua_tostring(L, 2);
+            int value = (int)lua_tonumber(L, 3);
+
+            if (varName == NULL) {
+                lua_pushboolean(L, 0); // Failure
+                return 1;
+            }
+
+            if (strcmp(varName, "VFXPlus") == 0) {
+                value ? Render3D::CMPatches_VFXPlus.Apply() : Render3D::CMPatches_VFXPlus.Restore();
+            }
+
+            lua_pushboolean(L, 1); // Success
+            return 1;
+        }
+
+        lua_pushnil(L);
+        return 1;
+    }
     void Init() {
 #if !RELOADED
         SafeWrite32(0x00A4EC84 + 4, (UInt32)&lua_func_never_die);
+        Logger::TypedLog("CHN_DBG", "address of lua func 0x%X \n", &lua_func_vint_get_avg_processing_time);
+        //static SafetyHookInline memoryutils = safetyhook::create_inline(0x00B907F0, &lua_func_vint_get_avg_processing_time);
+        SafeWrite32(0x00B91212 + 7, (UInt32)&lua_func_vint_get_avg_processing_time);
 #endif
     }
 }
