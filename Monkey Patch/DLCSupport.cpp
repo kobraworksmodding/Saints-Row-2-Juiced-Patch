aba7fe9221e4c74fbd3713685bd5002afb372bb6
Tervel1337 <78919226+Tervel1337@users.noreply.github.com>
2025-06-18 14:07:23 +0300

    Updated DLC audio hack - all dialogue will be covered now

--- Monkey Patch/DLCSupport.cpp
+++ Monkey Patch/DLCSupport.cpp
@@ -3,9 +3,10 @@
 #include <safetyhook.hpp>
 
 void LoadDLCPersonaVoice(SafetyHookContext& ctx) {
-	char* str = (char*)(ctx.esp + 76);
-	if (strcmp(str, "VOC_DL") == 0) { // this is a bit of a hack because it assumes you'll be using a voice_pc that marks the DLC audio as DL
-		strcpy_s(str, 4, "DLC");
+	char* str = (char*)(ctx.ebp);
+	const char* DLCBank = "SR2_DLC";
+	if (strcmp(str, "SR2_VOC_DL") == 0) { // this is a bit of a hack because it assumes you'll be using a voice_pc that marks the DLC audio as DL
+		ctx.ebp = (uintptr_t)DLCBank;
 	}
 }
 
@@ -15,6 +16,6 @@ void IncreaseMemPool() {
 }
 
 void DLCSetup() {
-	static SafetyHookMid DLCVoice = safetyhook::create_mid(0x006AD231, &LoadDLCPersonaVoice);
+	static SafetyHookMid DLCVoice = safetyhook::create_mid(0x0047AD09, &LoadDLCPersonaVoice);
 	IncreaseMemPool();
 }
