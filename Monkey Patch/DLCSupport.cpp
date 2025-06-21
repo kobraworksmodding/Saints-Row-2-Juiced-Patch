473578e855a04008375e70bf6209aaf2ab1a1e84
Tervel1337 <78919226+Tervel1337@users.noreply.github.com>
2025-06-21 16:35:05 +0300

    Load ui_dlc.peg into the interface backend memory pool

--- Monkey Patch/DLCSupport.cpp
+++ Monkey Patch/DLCSupport.cpp
@@ -15,7 +15,22 @@ void IncreaseMemPool() {
 	SafeWrite32((UInt32)0x006AD151, 2049100);
 }
 
+void __declspec(naked) AddInterfacePeg()
+{
+    static const char* DLC = "ui_dlc.peg";
+    static int jmp_continue = 0x00520803;
+    __asm {
+        mov edi, dword ptr[0x522450]
+        call edi
+        mov edx, 0x27716E4
+        mov ecx, DLC
+        call edi
+        jmp jmp_continue
+    }
+}
+
 void DLCSetup() {
+    WriteRelJump(0x005207FE, (UInt32)&AddInterfacePeg);
 	static SafetyHookMid DLCVoice = safetyhook::create_mid(0x0047AD09, &LoadDLCPersonaVoice);
 	IncreaseMemPool();
 }
