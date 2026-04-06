2fbe3b1edf771037734a6134fb87573ad401946e
Clippy95 <qpzezoqp15@gmail.com>
2026-04-06 21:39:52 +0300

    MiniDumpWithDataSegs for MiniDump

--- Monkey Patch/exception.hpp
+++ Monkey Patch/exception.hpp
@@ -739,12 +739,7 @@ LONG WINAPI CustomUnhandledExceptionFilter(LPEXCEPTION_POINTERS ExceptionInfo)
         ex.ExceptionPointers = ExceptionInfo;
         ex.ClientPointers = TRUE;
 
-        if (FAILED(MiniDumpWriteDump(GetCurrentProcess(), GetCurrentProcessId(), hFile,
-            (MINIDUMP_TYPE)(MiniDumpNormal |
-                MiniDumpWithThreadInfo |
-                MiniDumpWithIndirectlyReferencedMemory |
-                MiniDumpWithUnloadedModules |
-                MiniDumpWithProcessThreadData), &ex, NULL, NULL)))
+        if (FAILED(MiniDumpWriteDump(GetCurrentProcess(), GetCurrentProcessId(), hFile, MiniDumpWithDataSegs, &ex, NULL, NULL)))
 
         CloseHandle(hFile);
     }
