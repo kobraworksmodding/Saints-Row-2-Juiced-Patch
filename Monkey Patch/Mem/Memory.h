2c4e3fcb52f7988b5b3b47e7cbf5a344dd899ee4
Clippy95 <qpzezoqp15@gmail.com>
2026-04-16 00:11:53 +0300

    Revert "macroify mempool patching, add perm mesh gpu"
    This reverts commit 992da9611c54273b5115e3c235701a3e2d56ef7b.

--- Monkey Patch/Mem/Memory.h
+++ Monkey Patch/Mem/Memory.h
@@ -1,13 +1,6 @@
 #pragma once
-#include <vector>
 namespace Memory
 {
 
 	extern void Init();
 }
-
-struct mempool_entry_s {
-	const char* name;
-	bool updated_this_session = false;
-};
-inline std::vector<mempool_entry_s> mempool_registry;
