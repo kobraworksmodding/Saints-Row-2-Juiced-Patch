83123b4bdc7a53f2a7425edaba87e3a59fea65c9
Clippy95 <qpzezoqp15@gmail.com>
2026-05-01 13:29:08 +0300

    final fixing and cleanup

--- Monkey Patch/Network/MPStorage.cpp
+++ Monkey Patch/Network/MPStorage.cpp
@@ -19,26 +19,21 @@
 
 namespace MPStorage
 {
-
-	struct sr2_mp_player_data
-	{
-		int data[0xEE8C / 4];
-	};
-	static_assert(sizeof(sr2_mp_player_data) == 0xEE8C,"Wrong size for sr2_mp_player_data");
+	constexpr uint16_t GAME_MP_DATA_VERSION = 12;
+	constexpr size_t MP_STORAGE_DATA_SIZE = 0xEE8C;
+	constexpr int MP_STORAGE_STATUS_BLANK = 1;
+	constexpr int MP_STORAGE_STATUS_VALID = 2;
+	constexpr int MP_ASYNC_RESULT_OK = 0;
+	constexpr int MP_ASYNC_RESULT_FAILED = 2;
+	constexpr int MP_CALLBACK_STATUS_OK = 0;
+	constexpr int MP_CALLBACK_STATUS_BLANK = 1;
 
 	struct sr2_mp_data
 	{
 		uint16_t version;
-		int current_cash;
-		int lifetime_cash;
-		uint8_t mp_rank;
-		int mp_badge;
-		uint32_t server_upstream;
-		bool has_been_server;
-		sr2_mp_player_data creation_data;
-		uint8_t term_byte;
+		std::byte raw[MP_STORAGE_DATA_SIZE - sizeof(version)];
 	};
-	static_assert(sizeof(sr2_mp_data) == 0xEEAC, "Wrong size for sr2_mp_data");
+	static_assert(sizeof(sr2_mp_data) == MP_STORAGE_DATA_SIZE, "Wrong size for sr2_mp_data");
 
 	struct MPDT
 	{
@@ -50,17 +45,42 @@ namespace MPStorage
 	};
 
 	sr2_mp_data* mp_data = (sr2_mp_data*)DynAddress(0x02A18A08);
+	int* mp_storage_status = (int*)DynAddress(0x02528C24);
+	bool* mp_storage_async_pending = (bool*)DynAddress(0x02528C2C);
+	int* mp_async_callback_status = (int*)DynAddress(0x0212F46C);
+	int* mp_async_result = (int*)DynAddress(0x0212F470);
 	const char* where_to_save = (const char*)0x0144E650;
 
-	void SetOnlineStuff(int thing1, int thing2) 
+	void FinishAsyncRequest(int result, int callback_status)
 	{
-		cdecl_call<void>(0x81A3F0, thing1, thing2);
-		*(bool*)(0x2528C2C) = true;
+		*mp_async_result = result;
+		*mp_async_callback_status = callback_status;
+		*mp_storage_async_pending = false;
 	}
 
-	void SUCESSTHING()
+	void MarkBlankRead()
 	{
-		SetOnlineStuff(1, 1);
+		*mp_storage_status = MP_STORAGE_STATUS_BLANK;
+		FinishAsyncRequest(MP_ASYNC_RESULT_OK, MP_CALLBACK_STATUS_BLANK);
+	}
+
+	void MarkSuccessfulRead()
+	{
+		mp_data->version = GAME_MP_DATA_VERSION;
+		*mp_storage_status = MP_STORAGE_STATUS_VALID;
+		FinishAsyncRequest(MP_ASYNC_RESULT_OK, MP_CALLBACK_STATUS_OK);
+	}
+
+	void MarkSuccessfulWrite()
+	{
+		mp_data->version = GAME_MP_DATA_VERSION;
+		*mp_storage_status = MP_STORAGE_STATUS_VALID;
+		FinishAsyncRequest(MP_ASYNC_RESULT_OK, MP_CALLBACK_STATUS_OK);
+	}
+
+	void MarkWriteFailure()
+	{
+		FinishAsyncRequest(MP_ASYNC_RESULT_FAILED, MP_CALLBACK_STATUS_OK);
 	}
 
 	constexpr uint32_t MPDT_SIGNATURE = 'TDPM'; // "MPDT" little-endian
@@ -74,59 +94,55 @@ namespace MPStorage
 		return base / FOLDER_NAME / "mp" / "MPStorage.mpdt_pc";
 	}
 
-	bool get_data_from_server()
+	int __cdecl get_data_from_server(...)
 	{
 		auto path = GetMPStoragePath();
-		*(bool*)(0x2528C2C) = true;
-		if (!std::filesystem::exists(path))
-			return false;
+		std::error_code ec;
+		if (!std::filesystem::exists(path, ec) || ec)
+		{
+			MarkBlankRead();
+			Logger::TypedLog(CHN_NET, "MPStorage: no local storage found at {}, using blank storage.\n", path.string());
+			return 1;
+		}
 
 		std::ifstream file(path, std::ios::binary);
-		if (!file)
-			return false;
+		if (!file) {
+			Logger::TypedLog(CHN_NET, "MPStorage: failed to open {} for reading.\n", path.string());
+			return MP_ASYNC_RESULT_FAILED;
+		}
 
 		MPDT header{};
 		file.read(reinterpret_cast<char*>(&header), sizeof(MPDT));
 
-		if (!file)
-			return false;
-
-		if (header.signature != MPDT_SIGNATURE)
-			return false;
-
-		if (header.version != MPDT_VERSION)
-			return false;
-
-		if (header.offset_for_where_data_starts != offsetof(MPDT, data))
-			return false;
-
-		if (header.sr2_mp_data_size != sizeof(sr2_mp_data))
-			return false;
-
+		if (!file
+			|| header.signature != MPDT_SIGNATURE
+			|| header.version != MPDT_VERSION
+			|| header.offset_for_where_data_starts != offsetof(MPDT, data)
+			|| header.sr2_mp_data_size != sizeof(sr2_mp_data))
+		{
+			MarkBlankRead();
+			Logger::TypedLog(CHN_NET, "MPStorage: {} was invalid, using blank storage.\n", path.string());
+			return 1;
+		}
 
 		std::memcpy(mp_data, &header.data, sizeof(sr2_mp_data));
+		MarkSuccessfulRead();
+		Logger::TypedLog(CHN_NET, "MPStorage: loaded local player data from {}.\n", path.string());
 
-		SUCESSTHING();
-
-		return true;
+		return 1;
 	}
 
-	bool wtf_get_data_from_server()
+	bool save_data_to_server(char /*console_command*/)
 	{
-		auto result = get_data_from_server();
-		Logger::TypedLog("FUCKYOU", "THE THING IS UH {}\n", result);
-		return result;
-	}
-
-	bool save_data_to_server(char console_command)
-	{
-
 		auto path = GetMPStoragePath();
 
 		std::error_code ec;
 		std::filesystem::create_directories(path.parent_path(), ec);
-		if (ec)
+		if (ec) {
+			MarkWriteFailure();
+			Logger::TypedLog(CHN_NET, "MPStorage: failed to create directory for {}.\n", path.string());
 			return false;
+		}
 
 		MPDT out{};
 		out.signature = MPDT_SIGNATURE;
@@ -134,20 +150,31 @@ namespace MPStorage
 		out.offset_for_where_data_starts = offsetof(MPDT, data);
 		out.sr2_mp_data_size = sizeof(sr2_mp_data);
 
+		mp_data->version = GAME_MP_DATA_VERSION;
 		std::memcpy(&out.data, mp_data, sizeof(sr2_mp_data));
 
 		std::ofstream file(path, std::ios::binary | std::ios::trunc);
-		if (!file)
+		if (!file) {
+			MarkWriteFailure();
+			Logger::TypedLog(CHN_NET, "MPStorage: failed to open {} for writing.\n", path.string());
 			return false;
+		}
 
 		file.write(reinterpret_cast<const char*>(&out), sizeof(MPDT));
-		SUCESSTHING();
-		return file.good();
+		if (!file.good()) {
+			MarkWriteFailure();
+			Logger::TypedLog(CHN_NET, "MPStorage: failed to write {}.\n", path.string());
+			return false;
+		}
+
+		MarkSuccessfulWrite();
+		Logger::TypedLog(CHN_NET, "MPStorage: saved local player data to {}.\n", path.string());
+		return true;
 	}
 
 	void Init()
 	{
-		InjectHook(0x81A7A3, wtf_get_data_from_server);
+		InjectHook(0x81A7A3, get_data_from_server);
 		InjectHook(0x81A590, save_data_to_server, HookType::Jump);
 	}
 
