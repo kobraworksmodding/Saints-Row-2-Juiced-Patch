657b2f2317511706a9a5a31e288dffa67f761f87
Clippy95 <qpzezoqp15@gmail.com>
2026-05-02 00:05:46 +0300

    voice script only parsing

--- Monkey Patch/UGC/MStrings.cpp
+++ Monkey Patch/UGC/MStrings.cpp
@@ -33,6 +33,7 @@ void* __fastcall mempool_localization_align_alloc(size_t size, uint32_t align)
 }
 
 ankerl::unordered_dense::map<uint32_t, wchar_t*> g_CustomStrings;
+ankerl::unordered_dense::map<uint32_t, std::wstring> g_CustomVoiceStrings;
 
 namespace MStrings
 {
@@ -140,8 +141,8 @@ namespace MStrings
 		if (!raw)
 			return nullptr;
 
-		// This is important:
-		// text_ptr - 4 should be the live localization hash.
+		// this is important !!! the function that reads this shit is inlined D: 
+		// text_ptr - 4 should be the localization hash
 		*reinterpret_cast<uint32_t*>(raw) = hash;
 
 		wchar_t* text_ptr = reinterpret_cast<wchar_t*>(raw + sizeof(uint32_t));
@@ -196,12 +197,9 @@ namespace MStrings
 
 			std::string trimmed = Trim(line);
 
-			// Empty lines are ignored unless they are inside text.
 			if (trimmed.empty())
 				continue;
 
-			// New tag section:
-			// [Skip Intro]
 			if (trimmed.size() >= 2 && trimmed.front() == '[' && trimmed.back() == ']')
 			{
 				FlushEntry();
@@ -226,11 +224,35 @@ namespace MStrings
 		return true;
 	}
 
+	bool isCutscene()
+	{
+		return *(bool*)0x02527CF7 || *(bool*)0x02527D14_g;
+	}
+
+	wchar_t* GetCustomVoiceString(uint32_t checksum)
+	{
+		if (!checksum)
+			return nullptr;
+
+		auto it = g_CustomVoiceStrings.find(checksum);
+		if (it == g_CustomVoiceStrings.end())
+			return nullptr;
+
+		return const_cast<wchar_t*>(it->second.c_str());
+	}
+
 	SafetyHookInline get_localization_from_hashD;
 	wchar_t* _cdecl get_localization_from_hash(uint32_t checksum)
 	{
 		if (checksum)
 		{
+			if (isCutscene())
+			{
+				auto result = GetCustomVoiceString(checksum);
+				if (!result)
+					return result;
+			}
+
 			auto it = g_CustomStrings.find(checksum);
 			if (it != g_CustomStrings.end())
 				return it->second;
@@ -269,15 +291,10 @@ namespace MStrings
 
 			for (auto& s : parsed)
 			{
-				// Allocate into Localization_static_mempool style memory.
 				wchar_t* text_ptr = AllocLocalizationString(s.Hash, s.Text);
 
 				if (!text_ptr)
 					continue;
-
-				// Store this in your lookup map.
-				// Example:
-				// g_CustomStrings[s.Hash] = text_ptr;
 
 				g_CustomStrings[s.Hash] = text_ptr;
 			}
@@ -286,38 +303,175 @@ namespace MStrings
 		}
 	}
 
-
-	void StartScan()
+	struct ParsedVoiceString
 	{
-		const auto max_files = 1024;
-		const char** files = (const char**)calloc(max_files, sizeof(const char*));
-		if (files) {
-			auto number_of_files = cf_find_ext(files, max_files, ".anim_pc");
+		uint32_t Hash;
+		std::wstring Text;
+	};
 
-			if (number_of_files && files && *files) {
-				for (int i = 0; i < number_of_files; ++i)
-				{
-					Logger::TypedLog("MSTRINGS", "Found file {}\n", *files[i]);
-				}
+	static bool ParseVoiceCxtFile(const std::string& filepath, std::vector<ParsedVoiceString>& out)
+	{
+		std::ifstream file(filepath, std::ios::binary);
+		if (!file)
+			return false;
+
+		std::string line;
+		std::string current_tag;
+		std::string current_text;
+
+		auto FlushEntry = [&]()
+			{
+				if (current_tag.empty())
+					return;
+
+				uint32_t hash = Game::utils::str_to_hash(current_tag.c_str());
+
+				out.push_back({
+					hash,
+					Utf8ToWide(current_text)
+					});
+
+				current_tag.clear();
+				current_text.clear();
+			};
+
+		bool first_line = true;
+
+		while (std::getline(file, line))
+		{
+			if (!line.empty() && line.back() == '\r')
+				line.pop_back();
+
+			if (first_line)
+			{
+				StripUtf8Bom(line);
+				first_line = false;
 			}
-			if(files)
-			free(files);
-		}
-		GFile file;
-		file.OpenFile("charlist_JP.txt", "rb");
-		Logger::TypedLog("MSTRINGS", "Found file with size {}\n",file.GetSize() );
 
+			std::string trimmed = Trim(line);
+
+			if (trimmed.empty())
+				continue;
+
+			if (trimmed.size() >= 2 && trimmed.front() == '[' && trimmed.back() == ']')
+			{
+				FlushEntry();
+
+				current_tag = trimmed.substr(1, trimmed.size() - 2);
+				current_tag = Trim(current_tag);
+				continue;
+			}
+
+			if (!current_tag.empty())
+			{
+				if (!current_text.empty())
+					current_text += "\n";
+
+				current_text += line;
+			}
+		}
+
+		FlushEntry();
+
+		return true;
 	}
+
+	void StartParsingVoice()
+	{
+		const char* language_cstr = GetCurrentLanguage();
+		if (!language_cstr || !*language_cstr)
+			return;
+
+		std::string language = language_cstr;
+
+		// Match files like:
+		// anything_vcEN.cxt
+		// anything_vcJP.cxt
+		// voice_script_vcenglish.cxt
+		std::string wanted_suffix = "_vc" + language + ".cxt";
+
+		for (auto& entry : DirCache)
+		{
+			const std::string& filename = entry.first; // lowercase hopefully
+			const std::string& filepath = entry.second.FilePath;
+
+			if (!EndsWithInsensitive(filename, wanted_suffix))
+				continue;
+
+			std::vector<ParsedVoiceString> parsed;
+
+			if (!ParseVoiceCxtFile(filepath, parsed))
+				continue;
+
+			for (auto& s : parsed)
+			{
+				// Assignment copies the string into the map.
+				// Returned pointer will be g_CustomVoiceStrings[hash].c_str().
+				g_CustomVoiceStrings[s.Hash] = std::move(s.Text);
+			}
+
+			loaded_files_push_filename(filepath.c_str());
+		}
+	}
+
+	//void StartScan()
+	//{
+	//	const auto max_files = 1024;
+	//	const char** files = (const char**)calloc(max_files, sizeof(const char*));
+	//	if (files) {
+	//		auto number_of_files = cf_find_ext(files, max_files, ".anim_pc");
+
+	//		if (number_of_files && files && *files) {
+	//			for (int i = 0; i < number_of_files; ++i)
+	//			{
+	//				Logger::TypedLog("MSTRINGS", "Found file {}\n", *files[i]);
+	//			}
+	//		}
+	//		if(files)
+	//		free(files);
+	//	}
+	//	GFile file;
+	//	file.OpenFile("charlist_JP.txt", "rb");
+	//	Logger::TypedLog("MSTRINGS", "Found file with size {}\n",file.GetSize() );
+
+	//}
 	SafetyHookInline loadcharlistD;
 	char __cdecl loadcharlist()
 	{
 		auto result = loadcharlistD.unsafe_ccall<char>();
 		StartParsing();
+		StartParsingVoice();
+
+		if(!g_CustomStrings.empty() || !g_CustomVoiceStrings.empty())
+			get_localization_from_hashD = safetyhook::create_inline(0x7F4D60_g, get_localization_from_hash);
 		return result;
 	}
+	SafetyHookMid increase_localization_buffer;
 	void Init()
 	{
-		get_localization_from_hashD = safetyhook::create_inline(0x7F4D60_g, get_localization_from_hash);
 		loadcharlistD = safetyhook::create_inline(0x7F4430_g, loadcharlist);
+		increase_localization_buffer = safetyhook::create_mid(0x7F480E_g, [](SafetyHookContext& ctx) 
+			{
+				// theres a 100% better way to do this, I want to increase the mempool size, want it to estimate from our currnet custom strings (clippy95)
+				if (ctx.ebx == 0x02A15730_g)
+				{
+					uint32_t* max = (uint32_t*)((0x02A15730_g) + 0x3C);
+					uint32_t* end = (uint32_t*)((0x02A15730_g) + 0x4C);
+					uint32_t* end2 = (uint32_t*)((0x02A15730_g) + 0x50);
+					void** pool = (void**)((0x02A15730_g) + 0x38);
+					constexpr size_t new_localization_size = 1331200 * 4;
+					auto ptr = calloc(new_localization_size, 1);
+
+					if (ptr) 
+					{
+						*pool = ptr;
+						*max = new_localization_size;
+						*end = new_localization_size;
+						*end2 = new_localization_size;
+						(void)increase_localization_buffer.disable();
+					}
+				}
+
+			});
 	}
 }
