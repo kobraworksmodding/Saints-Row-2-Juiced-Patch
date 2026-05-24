eb7f97e6f2859d36ee7acf51794ef2ab3bf73f2e
Clippy95 <qpzezoqp15@gmail.com>
2026-05-24 12:38:20 +0300

    remove ExpandClothingLimit (due to it not increasing hash table size)

--- Monkey Patch/Mem/Memory.cpp
+++ Monkey Patch/Mem/Memory.cpp
@@ -39,84 +39,14 @@ namespace Memory
 		patchNop((BYTE*)0x0054DFDE, 15);
 	}
 
-	struct myItem
-	{
-		char bytes[120];
-	};
 	struct myLogoItem
 	{
 		char bytes[24];
 	};
-#define MAX_CUSTOM_ITEMS    3000
-	myItem NEW_ITEMS[MAX_CUSTOM_ITEMS];
 
 #define MAX_CUSTOM_LOGOS   24
 	myLogoItem NEW_LOGOS[MAX_CUSTOM_LOGOS];
 
-	void ExpandCustItemsPool()
-	{
-		//Patch Clothing limit from 1050 to 3000 BITCHES.
-
-		Logger::TypedLog(CHN_DEBUG, "Increasing Customization Memory from 1050 to 3000...\n");
-		//02A0A7F0
-		patchBytesM((BYTE*)0x007BBAC6 + 1, (BYTE*)"\x08\xBB", 2);
-		patchBytesM((BYTE*)0x007BCC14 + 6, (BYTE*)"\x08\xBB", 2);
-		patchDWord((BYTE*)0x0053FD41, (int)&NEW_ITEMS);
-		patchDWord((BYTE*)0x006109C5, (int)&NEW_ITEMS);
-		patchDWord((BYTE*)0x00611898, (int)&NEW_ITEMS);
-		patchDWord((BYTE*)0x00640F5C, (int)&NEW_ITEMS);
-		patchDWord((BYTE*)0x006BB709, (int)&NEW_ITEMS);
-		patchDWord((BYTE*)0x007BBAE1, (int)&NEW_ITEMS);
-		patchDWord((BYTE*)0x007BCDB7, (int)&NEW_ITEMS);
-		patchDWord((BYTE*)0x007BD67B, (int)&NEW_ITEMS);
-		patchDWord((BYTE*)0x007BDBF5, (int)&NEW_ITEMS);
-		patchDWord((BYTE*)0x007BE789, (int)&NEW_ITEMS);
-		patchDWord((BYTE*)0x007C1A4B, (int)&NEW_ITEMS);
-		patchDWord((BYTE*)0x007C1A86, (int)&NEW_ITEMS);
-		patchDWord((BYTE*)0x007C1AAD, (int)&NEW_ITEMS);
-		patchDWord((BYTE*)0x007C272C, (int)&NEW_ITEMS);
-		patchDWord((BYTE*)0x007C2CE6, (int)&NEW_ITEMS);
-		patchDWord((BYTE*)0x007C354F, (int)&NEW_ITEMS);
-		patchDWord((BYTE*)0x007C3BAF, (int)&NEW_ITEMS);
-		patchDWord((BYTE*)0x007C3E45, (int)&NEW_ITEMS);
-		patchDWord((BYTE*)0x007C3FA0, (int)&NEW_ITEMS);
-		patchDWord((BYTE*)0x007C5885, (int)&NEW_ITEMS);
-		patchDWord((BYTE*)0x007C5891, (int)&NEW_ITEMS);
-		patchDWord((BYTE*)0x007C58A7, (int)&NEW_ITEMS);
-		patchDWord((BYTE*)0x007C58B0, (int)&NEW_ITEMS);
-		patchDWord((BYTE*)0x007C6E97, (int)&NEW_ITEMS);
-		patchDWord((BYTE*)0x007C715F, (int)&NEW_ITEMS);
-		patchDWord((BYTE*)0x007C7366, (int)&NEW_ITEMS);
-		patchDWord((BYTE*)0x007C74D6, (int)&NEW_ITEMS);
-		patchDWord((BYTE*)0x007C8E2C, (int)&NEW_ITEMS);
-		patchDWord((BYTE*)0x007C8E6F, (int)&NEW_ITEMS);
-		patchDWord((BYTE*)0x007C8E7F, (int)&NEW_ITEMS);
-		patchDWord((BYTE*)0x007C8E88, (int)&NEW_ITEMS);
-		patchDWord((BYTE*)0x007CA32D, (int)&NEW_ITEMS);
-		patchDWord((BYTE*)0x007CA7C6, (int)&NEW_ITEMS);
-		patchDWord((BYTE*)0x007CA9CE, (int)&NEW_ITEMS);
-		patchDWord((BYTE*)0x007CB2D3, (int)&NEW_ITEMS);
-		patchDWord((BYTE*)0x007CB985, (int)&NEW_ITEMS);
-		patchDWord((BYTE*)0x007CC45C, (int)&NEW_ITEMS);
-		patchDWord((BYTE*)0x007CC826, (int)&NEW_ITEMS);
-		patchDWord((BYTE*)0x007CC865, (int)&NEW_ITEMS);
-		patchDWord((BYTE*)0x007CC87B, (int)&NEW_ITEMS);
-		patchDWord((BYTE*)0x007CC884, (int)&NEW_ITEMS);
-		patchDWord((BYTE*)0x007D43EA, (int)&NEW_ITEMS);
-		patchDWord((BYTE*)0x007D45CD, (int)&NEW_ITEMS);
-		patchDWord((BYTE*)0x007D641E, (int)&NEW_ITEMS);
-		patchDWord((BYTE*)0x0081ABF0, (int)&NEW_ITEMS);
-		patchDWord((BYTE*)0x0081ADB9, (int)&NEW_ITEMS);
-		patchDWord((BYTE*)0x0081B1B2, (int)&NEW_ITEMS);
-		patchDWord((BYTE*)0x0081B8BD, (int)&NEW_ITEMS);
-		patchDWord((BYTE*)0x0081C18B, (int)&NEW_ITEMS);
-		patchDWord((BYTE*)0x0081C6AA, (int)&NEW_ITEMS);
-		patchDWord((BYTE*)0x0081CBD2, (int)&NEW_ITEMS);
-		patchDWord((BYTE*)0x0081D828, (int)&NEW_ITEMS);
-		patchDWord((BYTE*)0x0081E102, (int)&NEW_ITEMS);
-		patchDWord((BYTE*)0x0088DE77, (int)&NEW_ITEMS);
-		patchDWord((BYTE*)0x009A02FE, (int)&NEW_ITEMS);
-	}
 
 	void PatchLogos() {
 		patchDWord((BYTE*)0x007BD198, (int)&NEW_LOGOS);
@@ -159,10 +89,6 @@ namespace Memory
 	void Init()
 	{
 		// ~ PatchLogos();
-	if (GameConfig::GetValue("Debug", "ExpandClothingLimit", 0, "Expands customization_items.xtbl limit to about 3050 items. Disable this if you're experiencing memory crashing issues with alot of clothing mods."))
-		{
-			ExpandCustItemsPool();
-		}
 #if RELOADED
 		if (GameConfig::GetValue("Debug", "GangstaBrawlMemoryExtender", 1)) // Replaces GB MemLimits with SA.
 		{
