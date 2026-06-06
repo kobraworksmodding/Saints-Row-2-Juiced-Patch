78e2eb5ddc4a9fd7701dab3fde0489727ca7bd59
Clippy95 <qpzezoqp15@gmail.com>
2026-06-06 18:04:09 +0300

    no LPF/muffle for bike radios (original game oversight)

--- Monkey Patch/Audio/Audio.cpp
+++ Monkey Patch/Audio/Audio.cpp
@@ -6,6 +6,8 @@
 #include "../Patcher/patch.h"
 #include "../GameConfig.h"
 #include "../SafeWrite.h"
+#include "usercaller.hpp"
+#include "safetyhook_inline.hpp"
 
 namespace Audio
 {
@@ -18,9 +20,35 @@ namespace Audio
 			patchBytesM((BYTE*)0x00DBA69C, (BYTE*)"\x00\x00\x00\x00", 4);
 		}
 	}
+	using vehicle_get_lpfABI = uc::abi<
+		uc::st0_ret<double>,
+		uc::eax_arg<int>,
+		uc::stack_arg<uintptr_t>,
+		uc::stack_arg<int>
+	>;
+	static uc::inline_hook<vehicle_get_lpfABI> vehicle_get_lpfD;
+
+	double UC_CDECL vehicle_get_lpf_hook(int a1, uintptr_t vehicle, int a3)
+	{
+		if (vehicle)
+		{
+			auto info = *(uintptr_t*)(vehicle + 0x84E4);
+			if (info)
+			{
+				// if bike
+				if (*(uintptr_t*)(info + 0x9C) == 1)
+					return 0.0;
+			}
+		}
+		return vehicle_get_lpfD.unsafe_call_original(a1, vehicle, a3);
+	}
 
 	void Init()
 	{
+		// so theres an oversight where bikes return a value as if it's a normal car rather than 0.0 (convertibles return 0.0!!!)
+		// causing music/radio to be muffled rather than not, not a PC bug also skipped over on console
+		// this of course does nothing if LPF isn't fixed which currently is broken on the PC release so there's really no difference - clippy95
+		vehicle_get_lpfD.create(0x483B00, vehicle_get_lpf_hook);
 		FixAudioDeviceAssignment();
 
 	}
